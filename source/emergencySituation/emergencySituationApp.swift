a11c0d1af715c2efc5dbd25d9604daec75a0ea55
howard.salmon <howard.salmon@kakaocorp.com>
2021-11-13 02:28:43 +0900

    init projects

---
+++ source/emergencySituation/emergencySituationApp.swift
@@ -0,0 +1,17 @@
+//
+//  emergencySituationApp.swift
+//  emergencySituation
+//
+//  Created by howard on 2021/11/13.
+//
+
+import SwiftUI
+
+@main
+struct emergencySituationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
